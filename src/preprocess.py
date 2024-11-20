2b4d30b35d98c1fc9cdd316cc13b0b99cc40d46a
Jacqueline Hong <jacqmhong@gmail.com>
2024-11-19 17:50:54 -0800

    Create clusters and conduct cluster analysis. Add PCA to preprocess.py for better embeddings.

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,15 +2,17 @@ import numpy as np
 import pandas as pd
 import pickle
 from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
 import yaml
 
 # Embedding configs
 with open("../config/config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
 
-model_name = config["embedding"]["model_name"] # SBERT
+model_name = config["embedding"]["model_name"]
 output_path = config["embedding"]["output_path"]
 batch_size = config["embedding"]["batch_size"]
+pca_components = config["embedding"]["pca_components"]
 
 # Dataset with the three categories: sentiment_label, conversation_stage, response_type
 df = pd.read_csv("../data/processed/labeled_sequential_convos.csv")
@@ -25,12 +27,26 @@ for i in range(0, len(df), batch_size):
 
 embeddings = np.array(embeddings)
 
-# Save the embeddings
+# PCA
+print(f"Applying PCA to reduce embeddings to {pca_components} dimensions.")
+pca = PCA(n_components=pca_components, random_state=0)
+reduced_embeddings = pca.fit_transform(embeddings)
+
+# Save reduced embeddings
+reduced_output_path = output_path.replace(".pkl", "_reduced.pkl")
+with open(reduced_output_path, 'wb') as f:
+    pickle.dump(reduced_embeddings, f)
+
+print(f"Reduced embeddings saved to {reduced_output_path}.")
+
+# Save the original embeddings (still used in cluster_analysis.ipynb to show the difference that PCA made)
 with open(output_path, 'wb') as f:
     pickle.dump(embeddings, f)
 
-print("Embedding generation completed and saved.")
+print(f"Original embeddings saved to {output_path}.")
 
 # Add embeddings to df
 df['embeddings'] = embeddings.tolist()
 df.to_csv("../data/processed/labeled_with_embeddings.csv", index=False)
+
+print("Embedding generation completed and saved.")
